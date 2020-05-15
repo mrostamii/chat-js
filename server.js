4051df06604261914862120a58405d6c129cdd16
M. Rostami <m.rostami9710@gmail.com>
2020-05-16 02:12:12 +0430

    src added

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http)
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+    console.log("Listening on port " + PORT)
+});
+
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname + "/index.html")
+})
+app.use(express.static('public'));
+
+
+io.on('connection', function(socket){
+    console.log("client has connected:" + socket.id)
+
+    socket.on('userMessage', (data) =>{
+        io.sockets.emit("userMessage", data)
+    })
+
+    socket.on('userTyping', (data) =>{
+        socket.broadcast.emit('userTyping', data)
+    });
+})
